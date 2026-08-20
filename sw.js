bb3e61595aefd0cd6882a2e4f8b521ef5b5c9842
gophisb <2.04622584e+08+gophisb@users.noreply.github.com>
2026-08-20 11:46:46 +0000

    Bust stale CSS cache for mobile azkar layout

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'rafeeq4-core-v15';
+const CACHE_NAME = 'rafeeq4-core-v16';
 const APP_SHELL = [
-  './', './index.html', './styles.css', './variables.css', './config.js', './prayer.js', './locations.js', './router.js', './app.js',
+  './', './index.html', './styles.css?v=16', './variables.css', './config.js', './prayer.js', './locations.js', './router.js', './app.js',
   './js/location-manager.js', './js/page-modules.js', './pages/home.html', './pages/home.js', './pages/quran.html', './pages/quran.js', './pages/quran-local.json', './pages/tafsir.html', './pages/tafsir.js', './pages/tafsir-saadi-local.json',
   './pages/azkar.html', './pages/azkar.js', './pages/azkar-data.json', './pages/nawawi.html', './pages/nawawi.js', './pages/nawawi-data.json', './pages/prayer.html', './pages/prayer.js', './pages/qibla.html', './pages/qibla.js', './pages/settings.html', './pages/settings.js', './manifest.json', './assets/icons/icon.svg', './assets/icons/icon-192.png', './assets/icons/icon-512.png', './assets/icons/apple-touch-icon.png', './assets/icons/kaaba.svg', './assets/icons/icon-quran.svg', './assets/icons/icon-quran.png', './assets/icons/icon-nawawi.svg', './assets/icons/icon-nawawi.png', './assets/icons/icon-azkar.svg', './assets/icons/icon-azkar.png', './assets/icons/icon-qibla.svg', './assets/icons/icon-qibla.png', './assets/audio/adhan.mp3'
 ];
